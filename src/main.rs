330a3bf3a62270a1e2a817fc06c50344c110bb07
Terrence Lam <skyuplam@gmail.com>
2018-02-14 21:50:10 +0100

    Get IP address with UDP method

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
 extern crate trust_dns_resolver;
 
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use trust_dns_resolver::Resolver;
 use trust_dns_resolver::config::*;
 
 
 fn main() {
     // Construct a new Resolver with default configuration options
-    let domain = None;
     let opendns_ns1 = NameServerConfig {
-        socker_addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(208,67,222,222)), 53),
+        socket_addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(208,67,222,222)), 53),
         protocol: Protocol::Udp,
     };
     let opendns_ns2 = NameServerConfig {
-        socker_addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(208,67,220,220)), 53),
+        socket_addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(208,67,220,220)), 53),
         protocol: Protocol::Udp,
     };
+    let domain = None;
+    let search = vec![];
+    let name_servers = vec![opendns_ns1, opendns_ns2];
     let config = ResolverConfig::from_parts(
         domain,
+        search,
+        name_servers,
     );
-    let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+    let resolver = Resolver::new(config, ResolverOpts::default()).unwrap();
 
     // Lookup the IP addresses associated with a name.
     // The final dot forces this to be an FQDN, otherwise the search rules as specified
     //  in `ResolverOpts` will take effect. FQDN's are generally cheaper queries.
-    let response = resolver.lookup_ip("www.example.com.").unwrap();
+    let response = resolver.lookup_ip("myip.opendns.com.").unwrap();
 
     // There can be many addresses associated with the name,
     //  this can return IPv4 and/or IPv6 addresses
     let address = response.iter().next().expect("no addresses returned!");
+    println!("Response: {}", address)
 }
