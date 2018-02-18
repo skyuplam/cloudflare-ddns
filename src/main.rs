76768c4f1aaed936ae0bab7080a799acf143bde2
Terrence Lam <skyuplam@gmail.com>
2018-02-18 22:30:19 +0100

    Add Logging and subcommands
    - Subcommands:
      + list
      + get
      + update
    - Logging to syslog and stdio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use] extern crate log;
+extern crate fern;
+extern crate syslog;
 extern crate trust_dns_resolver;
 #[macro_use] extern crate clap;
 extern crate futures;
@@ -18,12 +21,35 @@ use trust_dns_resolver::Resolver;
 use trust_dns_resolver::config::*;
 
 
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .chain(
+            // Console config
+            fern::Dispatch::new()
+                .level(log::LevelFilter::Debug)
+                .format(move |out, message, record| {
+                    out.finish(format_args!(
+                        "[{}] {}",
+                        record.level(),
+                        message,
+                    ))
+                })
+                .chain(std::io::stdout())
+        ).chain(
+            // Syslog config
+            fern::Dispatch::new()
+                .level(log::LevelFilter::Info)
+                .level_for("syslog", log::LevelFilter::Debug)
+                .chain(syslog::unix(syslog::Facility::LOG_USER)?)
+        ).apply()?;
+    Ok(())
+}
+
 fn dig_ip() -> IpAddr {
     // Construct a new Resolver with opendns resolvers
     let opendns_ns1 = NameServerConfig {
         socket_addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(208,67,222,222)), 53),
-        protocol: Protocol::Udp,
-    };
+        protocol: Protocol::Udp, };
     let opendns_ns2 = NameServerConfig {
         socket_addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(208,67,220,220)), 53),
         protocol: Protocol::Udp,
@@ -49,25 +75,38 @@ fn dig_ip() -> IpAddr {
 }
 
 fn main() {
+    // Setup Logging
+    setup_logger().unwrap();
+
     // Parse cli arg opts
     let matches = clap_app!(ddns =>
-        (version: "1.0")
-        (author: "Terrence Lam <skyuplam@gmail>")
+        (version: crate_version!())
+        (author: crate_authors!())
         (about: "Dynamically update DNS ip address")
         (@arg EMAIL: -e --email +takes_value +required "Auth Email")
         (@arg KEY: -k --key +takes_value +required "Auth Key")
-        (@arg NAME: -n --name +takes_value +required "DNS record name, e.g. example.com")
-        (@arg CONTENT: -c --content +takes_value "DNS record content, e.g. 127.0.0.1")
-        (@arg PROXIED: -p --proxied "Whether the record is proxied to cloudflare")
-        (@arg API_ENDPOINT: +required "API Endpoint for PUT DNS record")
+        (@arg ZONE_ID: -z --zoon_id +takes_value +required "Zone ID")
+        (@subcommand list =>
+            (about: "List all DNS records in the Zone")
+            (@arg NAME: "DNS record name, e.g. example.com")
+        )
+        (@subcommand get =>
+            (about: "Get the DNS record with provided ID")
+            (@arg RECORD_ID: +required "DNS record ID")
+        )
+        (@subcommand update =>
+            (about: "Update the DNS record with provided ID")
+            (@arg NAME: +required "DNS record name, e.g. example.com")
+            (@arg CONTENT: -c --content +takes_value "DNS record content, e.g. 127.0.0.1")
+            (@arg PROXIED: -p --proxied "Whether the record is proxied to cloudflare")
+        )
     ).get_matches();
 
     let email = matches.value_of("EMAIL").unwrap();
     let key = matches.value_of("KEY").unwrap();
-    let api_endpoint = matches.value_of("API_ENDPOINT").unwrap();
-    let record_name = matches.value_of("NAME").unwrap();
-    // let record_content = matches.value_of("CONTENT").unwrap_or("80.213.219.113");
-    let record_proxied = matches.is_present("PROXIED");
+    let zone_id = matches.value_of("ZONE_ID").unwrap();
+    let api_endpoint = format!("https://api.cloudflare.com/client/v4/zones/{}", zone_id);
+    // let record_proxied = matches.is_present("PROXIED");
 
     // create a tokio event loop
     let mut core = Core::new().unwrap();
@@ -76,39 +115,138 @@ fn main() {
         .connector(HttpsConnector::new(4, &handle).unwrap())
         .build(&handle);
 
-    // dig IP address
-    let address = dig_ip();
 
-    let record = json!({
-        "type": "A",
-        "name": record_name,
-        "content": address,
-        "proxied": record_proxied,
-    });
+    // Handle list subcommand
+    if let Some(list) = matches.subcommand_matches("list") {
+        let name = list.value_of("NAME").unwrap_or("");
+        let params = if name.is_empty() {
+            format!("")
+        } else {
+            format!("?name={}", name)
+        };
+        let uri = format!("{}/dns_records{}", api_endpoint, params);
+        // Make a GET request
+        let mut get_req = Request::new(Method::Get, uri.parse().unwrap());
+        get_req.headers_mut().set_raw("X-Auth-Email", email);
+        get_req.headers_mut().set_raw("X-Auth-Key", key);
 
-    // Make a PUT request
-    let mut req = Request::new(Method::Put, api_endpoint.parse().unwrap());
-    req.headers_mut().set(ContentType::json());
-    req.headers_mut().set_raw("X-Auth-Email", email);
-    req.headers_mut().set_raw("X-Auth-Key", key);
-    req.headers_mut().set(ContentLength(record.to_string().len() as u64));
-    req.set_body(record.to_string());
+        let get = client.request(get_req).and_then(|res| {
+            info!("List Response: {}", res.status());
 
-    let put = client.request(req).and_then(|res| {
-        println!("Response: {}", res.status());
+            res.body().concat2().and_then(move |body| {
+                let v: Value = serde_json::from_slice(&body).map_err(|e| {
+                    io::Error::new(io::ErrorKind::Other, e)
+                })?;
 
-        res.body().concat2().and_then(move |body| {
-            let v: Value = serde_json::from_slice(&body).map_err(|e| {
-                io::Error::new(
-                    io::ErrorKind::Other,
-                    e
-                )
-            })?;
-            println!("Success {}, Message {}, Error {}, Address: {}", v["success"], v["messages"], v["errors"], address);
-            Ok(())
-        })
-    });
+                if let Some(success) = v["success"].as_bool() {
+                    if !success {
+                        error!("Errors {}", v["errors"]);
+                    }
+                    info!("Result {}", v["result"]);
+                }
 
-    // Execute the request with tokio event loop
-    core.run(put).unwrap();
+                Ok(())
+            })
+        });
+        core.run(get).unwrap();
+    }
+
+    // Handle get subcommand
+    if let Some(get) = matches.subcommand_matches("get") {
+        let record_id = get.value_of("RECORD_ID").unwrap();
+        let uri = format!("{}/dns_records/{}", api_endpoint, record_id);
+        let mut get_req = Request::new(Method::Get, uri.parse().unwrap());
+        get_req.headers_mut().set_raw("X-Auth-Email", email);
+        get_req.headers_mut().set_raw("X-Auth-Key", key);
+        let get = client.request(get_req).and_then(|res| {
+            info!("Get Response: {}", res.status());
+
+            res.body().concat2().and_then(move |body| {
+                let v: Value = serde_json::from_slice(&body).map_err(|e| {
+                    io::Error::new(io::ErrorKind::Other, e)
+                })?;
+
+                if let Some(success) = v["success"].as_bool() {
+                    if !success {
+                        error!("Errors {}", v["errors"]);
+                    }
+                    info!("Result {}", v["result"]);
+                }
+
+                Ok(())
+            })
+        });
+        core.run(get).unwrap();
+    }
+
+    // Handle update subcommand
+    if let Some(update) = matches.subcommand_matches("update") {
+        let name = update.value_of("NAME").unwrap();
+        let content = update.value_of("CONTENT").unwrap_or("");
+        let uri = format!("{}/dns_records?name={}", api_endpoint, name);
+        let mut get_req = Request::new(Method::Get, uri.parse().unwrap());
+        get_req.headers_mut().set_raw("X-Auth-Email", email);
+        get_req.headers_mut().set_raw("X-Auth-Key", key);
+
+        let get = client.request(get_req).and_then(|res| {
+            info!("Get Response: {}", res.status());
+
+            res.body().concat2().and_then(move |body| {
+                let v: Value = serde_json::from_slice(&body).map_err(|e| {
+                    io::Error::new(io::ErrorKind::Other, e)
+                })?;
+
+                let success = v["success"].as_bool().unwrap();
+                if !success {
+                    panic!("Errors {}", v["errors"]);
+                }
+                let record_id = v["result"][0]["id"].as_str().unwrap();
+                Ok(record_id.to_string())
+            })
+        }).and_then(|record_id| {
+            // dig IP address
+            // let address = dig_ip();
+            let record_content = if content.is_empty() {
+                dig_ip()
+            } else {
+                content.parse().unwrap()
+            };
+
+            let record = json!({
+                "type": "A",
+                "name": name,
+                "content": record_content,
+                "proxied": update.is_present("PROXIED"),
+            });
+
+            // Make a PUT request
+            let uri = format!("{}/dns_records/{}", api_endpoint, record_id);
+            let mut put_req = Request::new(Method::Put, uri.parse().unwrap());
+            put_req.headers_mut().set(ContentType::json());
+            put_req.headers_mut().set_raw("X-Auth-Email", email);
+            put_req.headers_mut().set_raw("X-Auth-Key", key);
+            put_req.headers_mut().set(ContentLength(record.to_string().len() as u64));
+            put_req.set_body(record.to_string());
+
+            client.request(put_req).and_then(|res| {
+                res.body().concat2().and_then(move |body| {
+                    let v: Value = serde_json::from_slice(&body).map_err(|e| {
+                        io::Error::new(io::ErrorKind::Other, e)
+                    })?;
+
+                    if let Some(success) = v["success"].as_bool() {
+                        if !success {
+                            error!("Errors {}", v["errors"]);
+                        } else {
+                            info!("Result {}", v["result"]);
+                        }
+                    }
+
+                    Ok(())
+                })
+            })
+        });
+
+        core.run(get).unwrap();
+    }
 }
