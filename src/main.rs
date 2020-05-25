24cc80954ca4e48f484a460e3e6439e7bd04b580
Terrence Lam <skyuplam@gmail.com>
2020-05-25 11:13:34 +0200

    Add cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::str::FromStr;
 use tokio::net::UdpSocket;
 use tokio::task::spawn;
@@ -5,6 +6,11 @@ use tokio::task::spawn;
 use trust_dns_client::client::{AsyncClient, ClientHandle};
 use trust_dns_client::rr::{DNSClass, Name, RData, RecordType};
 use trust_dns_client::udp::UdpClientStream;
+
+use clap::{clap_app, crate_authors, crate_description, crate_name, crate_version};
+
+// https://api.cloudflare.com/
+const API_ENDPOINT: &str = "https://api.cloudflare.com/client/v4";
 
 /// DNS lookup with CH Class and TXT RecordType from Cloudflare's DNS 1.1.1.1
 /// and Name server `whoami.cloudflare`
@@ -52,5 +58,27 @@ async fn dig() -> Result<String, Box<dyn std::error::Error>> {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ip_addr = dig().await?;
     println!("{:#?}", ip_addr);
+    let matches = clap_app!(app =>
+        (name: crate_name!())
+        (version: crate_version!())
+        (author: crate_authors!())
+        (about: crate_description!())
+        (@arg API_KEY: --api_key +takes_value +required env("DDNS_API_KEY") "API key generated on the 'My Account' page")
+        (@arg EMAIL: --email +takes_value +required env("DDNS_EMAIL") "Email address associated with your account")
+        (@arg USER_SERVICE_KEY:
+            --service_key
+            +takes_value
+            +required
+            env("DDNS_SERVICE_KEY")
+            "A special Cloudflare API key good for a restricted set of endpoints. Always begins with 'v1.0-', may vary in length."
+        )
+        (@arg ZONE_ID: --zone +takes_value +required env("DDNS_ZONE") "Zone Identifier")
+        (@arg NAME: +required "DNS record name")
+    )
+    .get_matches();
+
+    let api_key = matches.value_of("API_KEY").unwrap();
+    println!("{}", api_key);
+
     Ok(())
 }
