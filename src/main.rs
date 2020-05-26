66b5d2575ed14395884def87fd405556a2c38100
Terrence Lam <skyuplam@gmail.com>
2020-05-26 17:38:17 +0200

    Add DNS record update logic

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use trust_dns_client::rr::{DNSClass, Name, RData, RecordType};
 use trust_dns_client::udp::UdpClientStream;
 
 use clap::{clap_app, crate_authors, crate_description, crate_name, crate_version};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 // https://api.cloudflare.com/
 const API_ENDPOINT: &str = "https://api.cloudflare.com/client/v4";
@@ -55,11 +55,10 @@ async fn dig() -> Result<String, Box<dyn std::error::Error>> {
     Ok(record)
 }
 
-
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct DNSRecord {
     id: String,
-    #[serde(rename="type")]
+    #[serde(rename = "type")]
     record_type: String,
     name: String,
     content: String,
@@ -73,23 +72,37 @@ struct DNSRecord {
     modified_on: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
+struct DNSUpdateRecord {
+    #[serde(rename = "type")]
+    record_type: String,
+    name: String,
+    content: String,
+    ttl: u8,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct RequestError {
     code: String,
     message: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct ListResponse {
     success: bool,
     result: Option<Vec<DNSRecord>>,
-    errors: Vec<RequestError>
+    errors: Vec<RequestError>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RecordResponse {
+    success: bool,
+    result: Option<DNSRecord>,
+    errors: Vec<RequestError>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ip_addr = dig().await?;
-    println!("{:#?}", ip_addr);
     let matches = clap_app!(app =>
         (name: crate_name!())
         (version: crate_version!())
@@ -113,9 +126,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let dns_records_endpoint = format!("{}/zones/{}/dns_records", API_ENDPOINT, zone);
 
-    let res = reqwest::Client::new()
+    let list_res: ListResponse = reqwest::Client::new()
         .get(dns_records_endpoint.as_str())
-        .header("Authorization", format!("Bearer {}", api_token))
+        .bearer_auth(api_token)
         .header("Content-Type", "application/json")
         .query(&[("name", name)])
         .send()
@@ -123,7 +136,53 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .json::<ListResponse>()
         .await?;
 
-    println!("{:#?}", res);
+    match list_res.success {
+        true => {
+            if let Some(records) = list_res.result {
+                match records.as_slice() {
+                    [] => println!("Record \"{}\" not found!", name),
+                    [record, ..] => {
+                        let current_ip = dig().await?;
+                        if current_ip == record.content {
+                            println!("Same IP, do nothing!");
+                        } else {
+                            let new_record = DNSUpdateRecord {
+                                record_type: record.record_type.clone(),
+                                name: record.name.clone(),
+                                content: current_ip,
+                                ttl: record.ttl,
+                            };
+                            let dns_record_update_endpoint =
+                                format!("{}/{}", dns_records_endpoint, record.id);
+                            let res: RecordResponse = reqwest::Client::new()
+                                .put(dns_record_update_endpoint.as_str())
+                                .bearer_auth(api_token)
+                                .header("Content-Type", "application/json")
+                                .json(&new_record)
+                                .send()
+                                .await?
+                                .json::<RecordResponse>()
+                                .await?;
+                            if res.success {
+                                match res.result {
+                                    Some(updated_record) => {
+                                        println!(
+                                            "DNS Record update success, {:#?}",
+                                            updated_record
+                                        );
+                                    }
+                                    None => println!("Update success but no return record..."),
+                                }
+                            } else {
+                                println!("{:#?}", res.errors);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        false => println!("{:#?}", list_res.errors),
+    }
 
     Ok(())
 }
